f2e33b08e7fcb9441f2d60afd0814bd117e28e5c
chenAmrani <chenamrani5@gmail.com>
2023-08-14 13:29:12 +0300

    chen and nir changes

--- Front/data.js
+++ Front/data.js
@@ -1,26 +1,27 @@
 
-// let shopItemsData;
-// function fetchDataFromServer(response){
-//     console.log(response);
-//     shopItemsData = response;
-//     console.log(shopItemsData);
 
-// }
+let shopItemsData=[];
+function fetchDataFromServer(response){
+    // console.log(response);
+    shopItemsData = response;
+    // console.log(shopItemsData);
+}
 
-// async function fetchDataAsync() {
-//     try {
-//         const response = await $.ajax({
-//             url: "http://localhost:3200/",
-//             type: "GET",
-//             //data: data,
-//         });
+async function fetchDataAsync() {
+    try {
+        const response = await $.ajax({
+            url: "http://localhost:1113/Movies",
+            type: "GET",
+            //data: data,
+        });
 
-//         await fetchDataFromServer(response);
-//     } catch (error) {
-//         console.error("Error fetching data:", error);
-//     }
-// }
-// fetchDataAsync();
+        await fetchDataFromServer(response);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+fetchDataAsync();
+
 
 
 
@@ -31,66 +32,25 @@
 
 
 
-let shopItemsData = [{
-    id:"movie1",
-    name: "movie1",
-    price:45,
-    desc:"movie1",
-    year: 1999,
-    rating: "rating : 4 " ,
-    actors: "chen,ori,nir,dorin",
-    img: "/imges/image1.jpg"
-},
-{
-    id:"movie2",
-    name: "movie2",
-    price: 25,
-    desc:"movie2",
-    year: 2002,
-    rating: "rating : 7 ",
-    actors: " actors : chen,ori,nir,dorin",
-    img: "/imges/image2.jpg"
-},
-{
-id:"movie3",
-name: "movie3",
-price: 15,
-desc:"movie3",
-year: 2022,
-rating: "rating : 3 ",
-actors: " actors : chen,ori,nir,dorin",
-img: "/imges/image3.jpg"
-},
-{
-id:"movie4",
-name: "movie4",
-price: 35,
-desc:"movie4",
-year: 1964,
-rating: "rating : 5 ",
-actors: " actors : chen,ori,nir,dorin",
-img: "/imges/image4.jpg"
-}
+// let shopItemsData = [{
+//     id:"movie1",
+//     name: "movie1",
+//     price:45,
+//     desc:"movie1",
+//     year: 1999,
+//     rating: "rating : 4 " ,
+//     actors: "chen,ori,nir,dorin",
+//     img: "/imges/image1.jpg"
 // },
 // {
-// id:"movie1",
-// name: "movie1",
-// price:45,
-// desc:"movie1",
-// year: 1999,
-// rating: "rating : 4 " ,
-// actors: "chen,ori,nir,dorin",
-// img: "/imges/image1.jpg"
-// },
-// {
-// id:"movie2",
-// name: "movie2",
-// price: 25,
-// desc:"movie2",
-// year: 2002,
-// rating: "rating : 7 ",
-// actors: " actors : chen,ori,nir,dorin",
-// img: "/imges/image2.jpg"
+//     id:"movie2",
+//     name: "movie2",
+//     price: 25,
+//     desc:"movie2",
+//     year: 2002,
+//     rating: "rating : 7 ",
+//     actors: " actors : chen,ori,nir,dorin",
+//     img: "/imges/image2.jpg"
 // },
 // {
 // id:"movie3",
@@ -112,4 +72,4 @@ img: "/imges/image4.jpg"
 // actors: " actors : chen,ori,nir,dorin",
 // img: "/imges/image4.jpg"
 // }
-];
+// ];
